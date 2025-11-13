2e52f1e6de79aa526510573a1de9bef2a8f34123
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-11-13 14:47:54 -0600

    Updated header UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import "./Header.css";
 
-function Header({ handleSignInClick, handleSignUpClick }) {
+function Header({ handleSignInClick, handleSignUpClick, isHomePage = true }) {
   return (
     <header className="header">
       <div className="header__title">News Explorer</div>
       <div className="header__navigation">
-        <button className="header__home-btn">Home</button>
+        <button className={`header__home-btn ${isHomePage ? "active" : ""}`}>
+          Home
+        </button>
         <button className="header__sign-in-btn" onClick={handleSignInClick}>
           Sign In
         </button>
